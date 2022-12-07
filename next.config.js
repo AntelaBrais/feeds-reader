bdc920d8659b85b41db533c2867b2bc3d9ce9ac4
Brais Antela <braantela@gmail.com>
2022-12-07 23:06:44 +0100

    :wrench: add next.js config with native-base support

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const { withExpo } = require("@expo/next-adapter")
+const withPlugins = require("next-compose-plugins")
+const withTM = require("next-transpile-modules")([
+	"react-native-web",
+	"react-native-svg",
+	"native-base",
+])
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = withPlugins(
+	[withTM, [withExpo, { projectRoot: __dirname }]],
+	nextConfig
+)
