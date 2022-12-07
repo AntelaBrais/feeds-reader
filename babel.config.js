f994d2bcb262a6da0a89f0ce2e85e8ee52fe6671
Brais Antela <braantela@gmail.com>
2022-12-07 23:05:35 +0100

    :wrench: update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: ["@expo/next-adapter/babel"],
+		plugins: [
+			["@babel/plugin-proposal-class-properties", { loose: true }],
+			["@babel/plugin-proposal-private-methods", { loose: true }],
+			["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+		],
+	}
+}
