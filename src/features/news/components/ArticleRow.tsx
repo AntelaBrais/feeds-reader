4555c02db1ddb2879605e206ca97c2ad11d61465
Brais Antela <braantela@gmail.com>
2022-12-08 17:23:16 +0100

    :sparkles: add `ArticleRow` component

---
+++ src/features/news/components/ArticleRow.tsx
@@ -0,0 +1,48 @@
+import { Link } from "@react-navigation/native"
+import { Row, Column, Image, Text } from "native-base"
+
+import { useSearchArticle } from "../hooks/useSearchArticle"
+
+export const ArticleRow = ({ article }) => {
+	const { articleTitleIncludesSearch } = useSearchArticle(article.title)
+
+	return (
+		<>
+			{article?.multimedia &&
+			article?.multimedia[0].caption &&
+			articleTitleIncludesSearch ? (
+				<Link
+					to={{ screen: "ArticleDetail", params: { articleUri: article.uri } }}>
+					<Row
+						width="full"
+						padding="2"
+						alignItems="center"
+						space="md"
+						borderBottomWidth="1"
+						borderBottomColor="muted.500">
+						<Image
+							source={{
+								uri: article?.multimedia ? article?.multimedia[0].url : "",
+							}}
+							alt={article?.multimedia ? article?.multimedia[0].caption : ""}
+							size="md"
+						/>
+						<Column flexGrow={1} flexBasis={0}>
+							<Text>{article.uri}</Text>
+							<Text
+								numberOfLines={2}
+								isTruncated
+								fontWeight="bold"
+								fontSize="sm">
+								{article.title}
+							</Text>
+							<Text numberOfLines={2} isTruncated fontSize="sm" maxWidth="full">
+								{article.abstract}
+							</Text>
+						</Column>
+					</Row>
+				</Link>
+			) : null}
+		</>
+	)
+}
