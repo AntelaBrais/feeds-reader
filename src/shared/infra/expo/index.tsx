e55589a9eb2650ee60746da4cdf795dedc1fe2a4
Brais Antela <braantela@gmail.com>
2022-12-08 22:45:24 +0100

    :sparkles: add store persistence/offline support

--- src/shared/infra/expo/index.tsx
+++ src/shared/infra/expo/index.tsx
@@ -1,9 +1,11 @@
 import * as Expo from "expo"
 import { StatusBar } from "expo-status-bar"
+import { PersistGate } from "redux-persist/integration/react"
 
 import { NativeBaseProvider } from "../providers/native-base"
 import { NavigationProvider } from "../providers/navigation"
 import { ReduxProvider } from "../providers/redux"
+import { persistor } from "../redux/configureStore"
 import { NativeNavigation } from "./navigation"
 
 const App = () => {
@@ -12,7 +14,9 @@ const App = () => {
 			<StatusBar style="auto" />
 			<NativeBaseProvider>
 				<ReduxProvider>
-					<NativeNavigation />
+					<PersistGate loading={null} persistor={persistor}>
+						<NativeNavigation />
+					</PersistGate>
 				</ReduxProvider>
 			</NativeBaseProvider>
 		</NavigationProvider>
--- src/shared/infra/redux/configureStore.tsx
+++ src/shared/infra/redux/configureStore.tsx
@@ -1,18 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import {
+	persistReducer,
+	persistStore,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PURGE,
+	REGISTER,
+	PERSIST,
+} from "redux-persist"
 
 import { apiSlice } from "../../../features/news/redux/apiSlice"
 import { articlesSliceReducer } from "../../../features/news/redux/articlesSlice"
 
-const rootReducer = {
+const rootReducer = combineReducers({
 	articles: articlesSliceReducer,
 	[apiSlice.reducerPath]: apiSlice.reducer,
+})
+
+const persistConfig = {
+	key: "root",
+	storage: AsyncStorage,
 }
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
 	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(apiSlice.middleware),
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}).concat(apiSlice.middleware),
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
