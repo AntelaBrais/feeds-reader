0236719581cedb32578132968f982158ea9902bf
Brais Antela <braantela@gmail.com>
2022-12-08 17:23:38 +0100

    :sparkles: add `SearchBar` component

---
+++ src/features/news/components/SearchBar.tsx
@@ -0,0 +1,34 @@
+import { Ionicons } from "@expo/vector-icons"
+import { VStack, Input, Icon } from "native-base"
+
+import { useSearchArticle } from "../hooks/useSearchArticle"
+
+export const SearchBar = () => {
+	const { searchText, handleSearchTextChange } = useSearchArticle()
+
+	return (
+		<VStack my="4" px="6" space="5" width="100%">
+			<VStack width="100%" space="5" alignSelf="center">
+				<Input
+					value={searchText}
+					onChangeText={handleSearchTextChange}
+					placeholder="Search by title"
+					variant="filled"
+					width="100%"
+					height="10"
+					borderRadius="10"
+					py="1"
+					px="2"
+					InputLeftElement={
+						<Icon
+							ml="2"
+							size="4"
+							color="gray.400"
+							as={<Ionicons name="ios-search" />}
+						/>
+					}
+				/>
+			</VStack>
+		</VStack>
+	)
+}
