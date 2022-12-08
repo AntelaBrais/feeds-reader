fed3b1a786e184f5de237327f9e49cfa766634bc
Brais Antela <braantela@gmail.com>
2022-12-08 17:26:01 +0100

    :sparkles: create `apiSlice` redux slice

---
+++ src/features/news/redux/apiSlice.ts
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+import { Article } from "../../../domain/articleEntity"
+
+const sortNewsByDate = (a, b) => {
+	const firstNewsDate = new Date(a.updated_date).getTime()
+	const secondNewsDate = new Date(b.updated_date).getTime()
+	return firstNewsDate - secondNewsDate
+}
+
+export const apiSlice = createApi({
+	reducerPath: "api",
+	baseQuery: fetchBaseQuery({
+		baseUrl: "https://api.nytimes.com/svc/topstories/v2/",
+	}),
+	endpoints: (builder) => ({
+		getNews: builder.query<Article[], void>({
+			query: () => ({
+				url: "arts.json",
+				params: {
+					"api-key": "wijSGhZSRGfsDwXeuaf2gEX9WkHvqywp",
+				},
+			}),
+			transformResponse: (response: { results: any[] }) => {
+				return response.results.sort(sortNewsByDate)
+			},
+		}),
+	}),
+})
+
+export const { useGetNewsQuery } = apiSlice
